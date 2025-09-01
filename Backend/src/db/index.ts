e0517d7a31c4fd06f2180e249de439197f8c3a6d
Dana-Klesly <dndonklesly@gmail.com>
2025-09-01 17:47:06 +0200

    feat: add conditional db interface for CI/CD

--- Backend/src/db/index.ts
+++ Backend/src/db/index.ts
@@ -1,4 +1,9 @@
-import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
+import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
 import { env } from "../env";
 
-export const db = drizzle(env.DATABASE_URL, { casing: "snake_case" });
+const isCI = process.env.CI === "true";
+
+export const db = isCI
+    ? drizzlePg(env.DATABASE_URL, { casing: "snake_case" })
+    : drizzleNeon(env.DATABASE_URL, { casing: "snake_case" });
